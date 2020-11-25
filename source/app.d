0694f8b006bc78fa28ad045f6666ded6d08a771c
sd <deryuzhovas@gmail.com>
2020-11-25 22:15:42 +0300

    fix potential fields

--- source/app.d
+++ source/app.d
@@ -4,16 +4,6 @@ import helper;
 
 void main()
 {
-	    auto p = Point2D!double(1,2);
-
-    auto p11 = Point2D!double(1,2);
-        auto p22 = Point2D!double(3,4);
-        auto p33 = p11+p22;
-        writeln(p33.dist());
-        assert(p33.x == 4);
-        assert(p33.y == 6);
-        p33+=p11;
-
 
     auto a = Point2D!double(0,2);
 	auto a2 = Point2D!double(3,0);
@@ -24,37 +14,67 @@ void main()
     // v.turn(3.14159/2);
 
 
-	int[][] matrix = [
-    [ 1, 2, 3 ],
-    [ 4, 5, 6 ]
-];
-assert(matrix[0][0] == 1);
-assert(matrix[0][1] == 2);
-
-
-auto ggg = array_generator(3,5);
+    auto InfMap = new InfluenceMap!double(3,5);
+//auto ggg = array_generator(3,5);
 //PutPotential!double(3,1,ggg,10,10,a);
 //PutPotential!double(3, 1, ggg, 10,10, b);
-PutPotential!double(3, 1, ggg, a);
+InfMap.PutPotential(3, 1, a);
 //PutPotential!double(3, 1, ggg, a2);
 
-auto ff = getSumOfVectorOnInfluenseMap!double(Point2D!double(0,0),Point2D!double(1,0), ggg);
-auto dcc = GetMinPotentialByRadius!double(1, ggg, Point2D!double(0,0));
-auto ff2 = getSumOfVectorOnInfluenseMap!double(Point2D!double(0,0),dcc, ggg);
-// // PutAvgPotential!double(6.5, 0.5, ggg, 10,10, b);
-// for(int x=0;x<5;x++){
-//     for(int y=0;y<3;y++){
-//     write((ggg[y][x]).to!string~" ");
-//     }
-// 	writeln();
-// }
-writeln(ggg[dcc.x.to!ulong][dcc.y.to!ulong]);
+auto ff = InfMap.getSumOfVectorOnInfluenseMap!double(Point2D!double(0,0),Point2D!double(1,0));
+auto dcc = InfMap.GetMinPotentialByRadius!double(2, Point2D!double(0,0));
+auto ff2 = InfMap.getSumOfVectorOnInfluenseMap!double(Point2D!double(0,0),dcc);
 
-writeln(ggg[2][0]);
-foreach(ref rw ; ggg)
+writeln(InfMap.Map[dcc.x.to!ulong][dcc.y.to!ulong]);
+writeln(InfMap.Map[2][0]);
+foreach(ref rw ; InfMap.Map)
     {
         writeln(rw);
     }
+// 	    auto p = Point2D!double(1,2);
+
+//     auto p11 = Point2D!double(1,2);
+//         auto p22 = Point2D!double(3,4);
+//         auto p33 = p11+p22;
+//         writeln(p33.dist());
+//         assert(p33.x == 4);
+//         assert(p33.y == 6);
+//         p33+=p11;
+
+
+//     auto a = Point2D!double(0,2);
+// 	auto a2 = Point2D!double(3,0);
+// 	auto b = Point2D!double(0,0);
+// 	auto c = Point2D!double(1,1);
+//     //     auto b = Point2D!double(4,0);
+//     //     auto v = Vect2D!(double)(a,b);
+//     // v.turn(3.14159/2);
+
+
+// 	int[][] matrix = [
+//     [ 1, 2, 3 ],
+//     [ 4, 5, 6 ]
+// ];
+// assert(matrix[0][0] == 1);
+// assert(matrix[0][1] == 2);
+
+
+// auto ggg = array_generator(3,5);
+// //PutPotential!double(3,1,ggg,10,10,a);
+// //PutPotential!double(3, 1, ggg, 10,10, b);
+// PutPotential!double(3, 1, ggg, a);
+// //PutPotential!double(3, 1, ggg, a2);
+
+// auto ff = getSumOfVectorOnInfluenseMap!double(Point2D!double(0,0),Point2D!double(1,0), ggg);
+// auto dcc = GetMinPotentialByRadius!double(1, ggg, Point2D!double(0,0));
+// auto ff2 = getSumOfVectorOnInfluenseMap!double(Point2D!double(0,0),dcc, ggg);
+
+// writeln(ggg[dcc.x.to!ulong][dcc.y.to!ulong]);
+// writeln(ggg[2][0]);
+// foreach(ref rw ; ggg)
+//     {
+//         writeln(rw);
+//     }
 
 	writeln("Edit source/app.d to start your project.");
 }
--- source/helper/influence_map.d
+++ source/helper/influence_map.d
@@ -4,202 +4,187 @@ import helper;
 import std.math;
 import std.conv;
 
+class InfluenceMap (T){
+    private: 
+        T[][] infMap;
+        int height;
+        int width;
 
-
-auto array_generator(int height,int width) {
-    auto matr = new double[][](height,width);
-
-    foreach(ref rw ; matr)
-    {
-        foreach(ref item ; rw)
-        {
-            item = 0.0;
-        }
-    }
-    
-    return matr;
-}
-
-double signedMax(double a, double b)
-{
-    if(abs(a)>=abs(b))
-        return a;
-    else return b;
-}
-
-double getSign(double x)
-{
-    if (x>0)
-        return 1;
-    else return -1;
-}
-
-void PutPotential(T)(double power, double step, double[][] matr, Point2D!T p)
-{
-    ulong sizeY =matr.length;
-    ulong sizeX =matr[0].length;
-    double s = 0;
-    int x = (abs(floor((p.x).to!double))).to!int;
-    int y = (abs(floor((p.y).to!double))).to!int;
-    for(double l = 0;l<fabs(power);l=l+step, s++)
-    {
-        for(int temp = (y-s).to!int;temp<=s+y;temp++)
-        {
-            int tempArrMinX = (floor(x-s)).to!int;
-            int tempArrMaxX = (floor(x+s)).to!int;
-            if(tempArrMinX>=0 && tempArrMinX<sizeX && temp>=0 && temp<sizeY)
-                matr[temp][tempArrMinX]=signedMax(matr[temp][tempArrMinX],getSign(power)*(abs(power)-l));
-            if(tempArrMaxX<sizeX && tempArrMaxX>=0 && temp>=0 && temp<sizeY)
-                matr[temp][tempArrMaxX]=signedMax(matr[temp][tempArrMaxX],getSign(power)*(abs(power)-l));
-        }
-        for(int temp = (x-s+1).to!int;temp<=s+x-1;temp++)
-        {
-            int tempArrMinY = (floor(y-s)).to!int;
-            int tempArrMaxY = (floor(y+s)).to!int;
-            if(tempArrMinY>=0 && tempArrMinY<sizeY && temp>=0 && temp<sizeX)
-                matr[tempArrMinY][temp]=signedMax(matr[tempArrMinY][temp],getSign(power)*(abs(power)-l));
-            if(tempArrMaxY>=0 && tempArrMaxY<sizeY  && temp>=0 && temp<sizeX)
-                matr[tempArrMaxY][temp]=signedMax(matr[tempArrMaxY][temp],getSign(power)*(abs(power)-l));
-        }
-    }
-}
-
-
-// // void array_destroyer(double **ary, unsigned int dim1) {
-// //     for (int i = 0; i < dim1; i++) {
-// //         delete [] ary[i];
-// //     }
-// //     delete [] ary;
-// // }
-
-double putp(double oldV, double newV)
-{
-    if(oldV==80)
-        return 80;
-    else if(oldV == 0)
-        return newV;
-    else
-        return (oldV+newV)/2;
-
-}
-
-void PutAvgPotential(T)(double power, double step, double[][] matr, Point2D!T p)
-{
-    ulong sizeY =matr.length;
-    ulong sizeX =matr[0].length;
-    double s = 0;
-    int x = (abs(floor((p.x).to!double))).to!int;
-    int y = (abs(floor((p.y).to!double))).to!int;
-    for(double l = 0;l<fabs(power);l=l+step, s++)
-    {
-        for(int temp = (y-s).to!int;temp<=s+y;temp++)
-        {
-            int tempArrMinX = (floor(x-s)).to!int;
-            int tempArrMaxX = (floor(x+s)).to!int;
-            if(tempArrMinX>=0 && tempArrMinX<sizeX && temp>=0 && temp<sizeY && matr[tempArrMinX][temp]<80)
-                matr[temp][tempArrMinX]=putp(matr[temp][tempArrMinX],getSign(power)*(abs(power)-l));
-            if(tempArrMaxX<sizeX && tempArrMaxX>=0 && temp>=0 && temp<sizeY && matr[tempArrMaxX][temp]<80)
-                matr[temp][tempArrMaxX]=putp(matr[temp][tempArrMaxX],getSign(power)*(abs(power)-l));
-        }
-        for(int temp = (x-s+1).to!int;temp<=s+x-1;temp++)
-        {
-            int tempArrMinY = (floor(y-s)).to!int;
-            int tempArrMaxY = (floor(y+s)).to!int;
-            if(tempArrMinY>=0 && tempArrMinY<sizeY && temp>=0 && temp<sizeX && matr[temp][tempArrMinY]<80)
-                matr[tempArrMinY][temp]=putp(matr[tempArrMinY][temp],getSign(power)*(abs(power)-l));
-            if(tempArrMaxY>=0 && tempArrMaxY<sizeY  && temp>=0 && temp<sizeX && matr[temp][tempArrMaxY]<80)
-                matr[tempArrMaxY][temp]=putp(matr[tempArrMaxY][temp],getSign(power)*(abs(power)-l));
-        }
-    }
-}
-
-
-bool isCorrectCoordinate(int x, int y, ulong sizeX, ulong sizeY)
-{
-    if(x>=0 && x<sizeX && y>=0 && y<sizeY)
-        return true;
-    return false;
-}
-
-double getSumOfVectorOnInfluenseMap(T)(Point2D!T fromV, Point2D!T toV, double[][] matr){
-    ulong sizeY =matr.length;
-    ulong sizeX =matr[0].length;
-    double sum = 0;
-    int sqareStartX =  fmin(floor(fromV.x.to!double).to!int, floor(toV.x.to!double).to!int).to!int;
-    int sqareFinishX = fmax(ceil(fromV.x.to!double).to!int,ceil(toV.x.to!double).to!int).to!int;
-    int sqareStartY = fmin(floor(fromV.y.to!double).to!int,floor(toV.y.to!double).to!int).to!int;
-    int sqareFinishY = fmax(ceil(fromV.y.to!double).to!int,ceil(toV.y.to!double).to!int).to!int;
-
-    for(int i = sqareStartX; i<=sqareFinishX;i++)
-    {
-        for(int j = sqareStartY; j<=sqareFinishY;j++) {
-            if (isCorrectCoordinate(i, j, sizeX, sizeY) && (
-                    intersect(fromV, toV, Point2D!T(size_t(i), size_t(j)), Point2D!T(size_t(i), size_t(j) + 1))
-                    ||
-                    intersect(fromV, toV, Point2D!T(size_t(i), size_t(j)), Point2D!T(size_t(i) + 1, size_t(j)))
-                    ||
-                    intersect(fromV, toV, Point2D!T(size_t(i) + 1, size_t(j)),
-                              Point2D!T(size_t(i) + 1, size_t(j) + 1))
-                    ||
-                    intersect(fromV, toV, Point2D!T(size_t(i), size_t(j) + 1),
-                              Point2D!T(size_t(i) + 1, size_t(j) + 1))
-            )) {
-                sum += matr[j][i];
+        auto array_generator(int _height,int _width) {
+            auto matr = new double[][](_height,_width);
+            foreach(ref rw ; matr){
+                foreach(ref item ; rw){
+                    item = 0.0;
+                }
             }
-            else if(!isCorrectCoordinate(i, j, sizeX, sizeY))
-            {sum +=80;}
-        }
-    }
-    return sum;
-}
+            return matr;
+        }     
 
-Point2D!T GetMinPotentialByRadius(T)(int radius, double[][] matr,  Point2D!T source) {
-    ulong sizeY =matr.length;
-    ulong sizeX =matr[0].length;
-    double min = 10000;
-    Point2D!T minPos = source;
-    // int x = int(abs(floor(source.x)));
-    // int y = int(abs(floor(source.y)));
+        double signedMax(double a, double b){
+            if(abs(a)>=abs(b)) return a;
+            else return b;
+        }
 
-    int x = (abs(floor((source.x).to!double))).to!int;
-    int y = (abs(floor((source.y).to!double))).to!int;
+        double getSign(double x){
+            if (x>0) return 1;
+            else return -1;
+        }
 
-    for (int temp = y-radius+1; temp < y+radius; temp++) {
-        int minX = x-radius;
-        int maxX = x+radius;
-        double minSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (minX,temp), matr);
-        double maxSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (maxX,temp), matr);
-        if(minSum<min)
-        {
-            min = minSum;
-            minPos.x = minX;
-            minPos.y = temp;
+        double putp(double oldV, double newV){
+            if(oldV==80) return 80;
+            else if(oldV == 0) return newV;
+            else return (oldV+newV)/2;
         }
-        if(maxSum<min)
-        {
-            min = maxSum;
-            minPos.x = maxX;
-            minPos.y = temp;
+
+        bool isCorrectCoordinate(int x, int y){
+            if(x>=0 && x<this.width && y>=0 && y<this.height)
+                return true;
+            return false;
         }
-    }
-    for (int temp = x-radius+1; temp < x+radius; temp++) {
-        int minY = y-radius;
-        int maxY = y+radius;
-        double minSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (temp,minY), matr);
-        double maxSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (temp,maxY), matr);
-        if(minSum<min)
-        {
-            min = minSum;
-            minPos.x = temp;
-            minPos.y = minY;
+
+    public: 
+        @property nothrow pure double[][] Map() { return infMap; }
+        @property nothrow pure int Height() { return height; }
+        @property nothrow pure int Width() { return width; }
+
+        this(int _height,int _width){
+            this.height = _height;
+            this.width = _width;
+            infMap = array_generator(_height,_width);
         }
-        if(maxSum<min)
+        
+        void PutPotential(double power, double step, Point2D!T p)
         {
-            min = maxSum;
-            minPos.x = temp;
-            minPos.y = maxY;
+            double s = 0;
+            int x = (abs(floor((p.x).to!double))).to!int;
+            int y = (abs(floor((p.y).to!double))).to!int;
+            for(double l = 0;l<fabs(power);l=l+step, s++)
+            {
+                for(int temp = (y-s).to!int;temp<=s+y;temp++)
+                {
+                    int tempArrMinX = (floor(x-s)).to!int;
+                    int tempArrMaxX = (floor(x+s)).to!int;
+                    if(isCorrectCoordinate(tempArrMinX, temp))
+                        infMap[temp][tempArrMinX]=signedMax(infMap[temp][tempArrMinX],getSign(power)*(abs(power)-l));
+                    if(isCorrectCoordinate(tempArrMaxX, temp))
+                        infMap[temp][tempArrMaxX]=signedMax(infMap[temp][tempArrMaxX],getSign(power)*(abs(power)-l));
+                }
+                for(int temp = (x-s+1).to!int;temp<=s+x-1;temp++)
+                {
+                    int tempArrMinY = (floor(y-s)).to!int;
+                    int tempArrMaxY = (floor(y+s)).to!int;
+                    if(isCorrectCoordinate(temp, tempArrMinY))
+                        infMap[tempArrMinY][temp]=signedMax(infMap[tempArrMinY][temp],getSign(power)*(abs(power)-l));
+                    if(isCorrectCoordinate(temp, tempArrMaxY))
+                        infMap[tempArrMaxY][temp]=signedMax(infMap[tempArrMaxY][temp],getSign(power)*(abs(power)-l));
+                }
+            }
         }
-    }
-    return minPos;
+
+        void PutAvgPotential(T)(double power, double step,  Point2D!T p, double wall=80)
+        {
+            ulong sizeY =matr.length;
+            ulong sizeX =matr[0].length;
+            double s = 0;
+            int x = (abs(floor((p.x).to!double))).to!int;
+            int y = (abs(floor((p.y).to!double))).to!int;
+            for(double l = 0;l<fabs(power);l=l+step, s++)
+            {
+                for(int temp = (y-s).to!int;temp<=s+y;temp++)
+                {
+                    int tempArrMinX = (floor(x-s)).to!int;
+                    int tempArrMaxX = (floor(x+s)).to!int;
+                    if(isCorrectCoordinate(tempArrMinX, temp) && matr[tempArrMinX][temp]<wall)
+                        infMap[temp][tempArrMinX]=putp(infMap[temp][tempArrMinX],getSign(power)*(abs(power)-l));
+                    if(isCorrectCoordinate(tempArrMaxX, temp) && matr[tempArrMaxX][temp]<wall)
+                        infMap[temp][tempArrMaxX]=putp(infMap[temp][tempArrMaxX],getSign(power)*(abs(power)-l));
+                }
+                for(int temp = (x-s+1).to!int;temp<=s+x-1;temp++)
+                {
+                    int tempArrMinY = (floor(y-s)).to!int;
+                    int tempArrMaxY = (floor(y+s)).to!int;
+                    if(isCorrectCoordinate(temp, tempArrMinY) && matr[temp][tempArrMinY]<wall)
+                        infMap[tempArrMinY][temp]=putp(infMap[tempArrMinY][temp],getSign(power)*(abs(power)-l));
+                    if(isCorrectCoordinate(temp, tempArrMaxY) && matr[temp][tempArrMaxY]<wall)
+                        infMap[tempArrMaxY][temp]=putp(infMap[tempArrMaxY][temp],getSign(power)*(abs(power)-l));
+                }
+            }
+        }
+
+        //TODO: пофиксить это убожество
+        double getSumOfVectorOnInfluenseMap(T)(Point2D!T fromV, Point2D!T toV, double wall=80){
+            double sum = 0;
+            int sqareStartX =  fmin(floor(fromV.x.to!double).to!int, floor(toV.x.to!double).to!int).to!int;
+            int sqareFinishX = fmax(ceil(fromV.x.to!double).to!int,ceil(toV.x.to!double).to!int).to!int;
+            int sqareStartY = fmin(floor(fromV.y.to!double).to!int,floor(toV.y.to!double).to!int).to!int;
+            int sqareFinishY = fmax(ceil(fromV.y.to!double).to!int,ceil(toV.y.to!double).to!int).to!int;
+
+            for(int i = sqareStartX; i<=sqareFinishX;i++)
+            {
+                for(int j = sqareStartY; j<=sqareFinishY;j++) {
+                    if (isCorrectCoordinate(i, j) && (
+                            intersect(fromV, toV, Point2D!T(size_t(i), size_t(j)), Point2D!T(size_t(i), size_t(j) + 1))
+                            ||
+                            intersect(fromV, toV, Point2D!T(size_t(i), size_t(j)), Point2D!T(size_t(i) + 1, size_t(j)))
+                            ||
+                            intersect(fromV, toV, Point2D!T(size_t(i) + 1, size_t(j)), Point2D!T(size_t(i) + 1, size_t(j) + 1))
+                            ||
+                            intersect(fromV, toV, Point2D!T(size_t(i), size_t(j) + 1), Point2D!T(size_t(i) + 1, size_t(j) + 1))
+                    )) {
+                        sum += infMap[j][i];
+                    }
+                    else if(!isCorrectCoordinate(i, j))
+                    {sum +=wall;}
+                }
+            }
+            return sum;
+        }
+
+        Point2D!T GetMinPotentialByRadius(T)(int radius, Point2D!T source) {
+            double min = 10000;
+            Point2D!T minPos = source;
+
+            int x = (abs(floor((source.x).to!double))).to!int;
+            int y = (abs(floor((source.y).to!double))).to!int;
+
+            for (int temp = y-radius+1; temp < y+radius; temp++) {
+                int minX = x-radius;
+                int maxX = x+radius;
+                double minSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (minX,temp));
+                double maxSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (maxX,temp));
+                if(minSum<min)
+                {
+                    min = minSum;
+                    minPos.x = minX;
+                    minPos.y = temp;
+                }
+                if(maxSum<min)
+                {
+                    min = maxSum;
+                    minPos.x = maxX;
+                    minPos.y = temp;
+                }
+            }
+            for (int temp = x-radius+1; temp < x+radius; temp++) {
+                int minY = y-radius;
+                int maxY = y+radius;
+                double minSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (temp,minY));
+                double maxSum = getSumOfVectorOnInfluenseMap(source, Point2D!T (temp,maxY));
+                if(minSum<min)
+                {
+                    min = minSum;
+                    minPos.x = temp;
+                    minPos.y = minY;
+                }
+                if(maxSum<min)
+                {
+                    min = maxSum;
+                    minPos.x = temp;
+                    minPos.y = maxY;
+                }
+            }
+            return minPos;
+
+        }
 
 }
